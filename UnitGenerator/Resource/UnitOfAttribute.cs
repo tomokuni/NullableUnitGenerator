ee3cd17c65e14893d56196c9f8e92b6af4e2b73a
tomokuni <tomokuni@users.noreply.github.com>
2023-05-11 19:07:44 +0900

    Update NullableUnitGenerator.csproj

--- UnitGenerator/Resource/UnitOfAttribute.cs
+++ UnitGenerator/Resource/UnitOfAttribute.cs
@@ -2,7 +2,6 @@
 #pragma warning disable CS8632	// '#nullable' 注釈コンテキスト内のコードでのみ、Null 許容参照型の注釈を使用する必要があります。
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace NullableUnitGenerator;
 
@@ -52,56 +51,75 @@ public partial class UnitOfOasAttribute : Attribute
     /// <summary>For OpenApiSchema.Format</summary>
     public string? Format { get; }
 
-    /// <summary>For OpenApiSchema.Format</summary>
-    public decimal? Maximum { get; }
-
-    /// <summary>For OpenApiSchema.Format</summary>
+    /// <summary>For OpenApiSchema.Minimum</summary>
     public decimal? Minimum { get; }
 
-    /// <summary>For OpenApiSchema.Format</summary>
-    public int? MaxLength { get; }
+    /// <summary>For OpenApiSchema.ExclusiveMinimum</summary>
+    public bool? ExclusiveMinimum { get; }
 
-    /// <summary>For OpenApiSchema.Format</summary>
+    /// <summary>For OpenApiSchema.Maximum</summary>
+    public decimal? Maximum { get; }
+
+    /// <summary>For OpenApiSchema.ExclusiveMaximum</summary>
+    public bool? ExclusiveMaximum { get; }
+
+    /// <summary>For OpenApiSchema.MultipleOf</summary>
+    public decimal? MultipleOf { get; }
+
+    /// <summary>For OpenApiSchema.MinLength</summary>
     public int? MinLength { get; }
+
+    /// <summary>For OpenApiSchema.MaxLength</summary>
+    public int? MaxLength { get; }
 
     /// <summary>For OpenApiSchema.Pattern</summary>
     public string? Pattern { get; }
 
-    /// <summary>For OpenApiSchema.Nullable</summary>
-    public bool Nullable { get; }
-
     /// <summary>For OpenApiSchema.Example</summary>
     public string? Example { get; }
+
+    /// <summary>For OpenApiSchema.Nullable</summary>
+    public bool Nullable { get; }
 
     /// <summary>
     /// コンストラクタ
     /// </summary>
-    /// <param name="type"></param>
-    /// <param name="format"></param>
-    /// <param name="maximum"></param>
-    /// <param name="minimum"></param>
-    /// <param name="maxLength"></param>
-    /// <param name="minLength"></param>
+    /// <param name="type">string, number, integer, boolean, ...</param>
+    /// <param name="format">type:number:(-, float, double), type:integer:(-, int32, int64)</param>
+    /// <param name="minimum">MaxValue is handled as null</param>
+    /// <param name="exclusiveMinimum"></param>
+    /// <param name="maximum">MinValue is handled as null</param>
+    /// <param name="exclusiveMaximum"></param>
+    /// <param name="multipleOf">Negative value is handled as null</param>
+    /// <param name="minLength">MaxValue is handled as null</param>
+    /// <param name="maxLength">MinValue is handled as null</param>
     /// <param name="pattern"></param>
     /// <param name="nullable"></param>
     /// <param name="example"></param>
+    /// <remarks>https://swagger.io/docs/specification/data-models/data-types/</remarks>
     public UnitOfOasAttribute(
-        [DisallowNull] string type,
-        [AllowNull] string? format = null,
-        [AllowNull] decimal? maximum = null,
-        [AllowNull] decimal? minimum = null,
-        [AllowNull] int? maxLength = null,
-        [AllowNull] int? minLength = null,
-        [AllowNull] string? pattern = null,
-        [AllowNull] string? example = null,
+        string type,
+        string? format = null,
+        decimal maximum = decimal.MinValue,
+        bool exclusiveMinimum = false,
+        decimal minimum = decimal.MinValue,
+        bool exclusiveMaximum = false,
+        decimal multipleOf = decimal.MinValue,
+        int maxLength = int.MinValue,
+        int minLength = int.MinValue,
+        string? pattern = null,
+        string? example = null,
         bool nullable = true)
     {
         Type = type;
         Format = format;
-        Maximum = maximum;
-        Minimum = minimum;
-        MaxLength = maxLength;
-        MinLength = minLength;
+        Minimum = (minimum == decimal.MaxValue) ? null : minimum;
+        ExclusiveMinimum = (minimum == decimal.MaxValue) ? null : exclusiveMinimum;
+        Maximum = (maximum == decimal.MinValue) ? null : maximum;
+        ExclusiveMaximum = (maximum == decimal.MinValue) ? null : exclusiveMaximum;
+        MultipleOf = (multipleOf < 0) ? null : multipleOf;
+        MinLength = (minLength == int.MaxValue) ? null : minLength;
+        MaxLength = (maxLength == int.MinValue) ? null : maxLength;
         Pattern = pattern;
         Nullable = nullable;
         Example = example;
