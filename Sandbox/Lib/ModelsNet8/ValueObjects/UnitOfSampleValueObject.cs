ac35634b3708c134abe45b03a9c72607c86e3b86
tomokuni <tomokuni@users.noreply.github.com>
2023-11-26 15:29:23 +0900

    change framework, .NET7 -> .NET8

---
+++ Sandbox/Lib/ModelsNet8/ValueObjects/UnitOfSampleValueObject.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using NullableUnitGenerator;
+using UGO = NullableUnitGenerator.UnitGenerateOption;
+
+namespace NullableUnitGeneratorSample;
+
+
+// Builtins
+
+[UnitOf(typeof(bool), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("boolean", example:"true")]
+public readonly partial struct VoBoolSample { }
+
+
+[UnitOf(typeof(char), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("string")]
+public readonly partial struct VoCharSample { }
+
+
+[UnitOf(typeof(byte), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoByteSample { }
+
+
+[UnitOf(typeof(sbyte), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoSbyteSample { }
+
+
+[UnitOf(typeof(short), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoShortSample { }
+
+
+[UnitOf(typeof(ushort), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoUshortSample { }
+
+
+[UnitOf(typeof(int), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoIntSample { }
+
+
+[UnitOf(typeof(uint), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoUintSample { }
+
+
+[UnitOf(typeof(long), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoLongSample { }
+
+
+[UnitOf(typeof(ulong), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("integer")]
+public readonly partial struct VoUlongSample { }
+
+
+[UnitOf(typeof(float), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("number")]
+public readonly partial struct VoFloatSample { }
+
+
+[UnitOf(typeof(double), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("number")]
+public readonly partial struct VoDoubleSample { }
+
+
+[UnitOf(typeof(decimal), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("number")]
+public readonly partial struct VoDecimalSample { }
+
+
+[UnitOf(typeof(string), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("string")]
+public readonly partial struct VoStringSample { }
+
+
+// Others
+
+[UnitOf(typeof(Guid), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("string")]
+public readonly partial struct VoGuidSample { }
+
+
+[UnitOf(typeof(Ulid), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("string")]
+public readonly partial struct VoUlidSample { }
+
+
+[UnitOf(typeof(DateTime), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("datetime")]
+public readonly partial struct VoDatetimeSample { }
+
+
+[UnitOf(typeof(DateOnly), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("date")]
+public readonly partial struct VoDateonlySample { }
+
+
+[UnitOf(typeof(TimeOnly), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("time")]
+public readonly partial struct VoTimeonlySample { }
+
+
+[UnitOf(typeof(TimeSpan), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("time")]
+public readonly partial struct VoTimespanSample { }
+
+
+
+[UnitOf(typeof(byte[]), UGO.GeneralOptions | UGO.JsonConverter | UGO.MessagePackFormatter | UGO.DapperTypeHandler | UGO.EntityFrameworkValueConverter)]
+[UnitOfOas("string")]
+public readonly partial struct VoByteArraySample { }  // base64 encoded characters
