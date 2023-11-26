864947058b45b260b732a4adfd0c0dc026c4026b
tomokuni <tomokuni@users.noreply.github.com>
2023-11-26 20:13:58 +0900

    一時削除 ModelsNet8

--- Sandbox/Lib/ModelsNet8/ValueObjects/Sandbox/EntityFrameworkApp.cs
+++
@@ -1,8 +0,0 @@
-﻿using NullableUnitGenerator;
-using UGO = NullableUnitGenerator.UnitGenerateOption;
-
-namespace NullableUnitGeneratorSample.Sandbox.EntityFrameworkApp;
-
-
-[UnitOf(typeof(int), UGO.ParseMethod | UGO.EntityFrameworkValueConverter)]
-public readonly partial struct UserId { }
--- Sandbox/Lib/ModelsNet8/ValueObjects/Sandbox/WebApiApp.cs
+++
@@ -1,8 +0,0 @@
-﻿using NullableUnitGenerator;
-using UGO = NullableUnitGenerator.UnitGenerateOption;
-
-namespace NullableUnitGeneratorSample.Sandbox.WebApiApp;
-
-
-[UnitOf(typeof(int), UGO.ParseMethod | UGO.EntityFrameworkValueConverter)]
-public readonly partial struct UserId { }
